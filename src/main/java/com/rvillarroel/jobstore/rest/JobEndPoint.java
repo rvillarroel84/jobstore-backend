2691a2bdf8cb489661b309e39e7345be75a9ec8c
Ronny <ronaldovm.villarroel@gmail.com>
2019-05-10 10:23:07 -0400

    Add Methos GET DELETE POST JAXRS

--- src/main/java/com/rvillarroel/jobstore/rest/JobEndPoint.java
+++ src/main/java/com/rvillarroel/jobstore/rest/JobEndPoint.java
@@ -1,13 +1,21 @@
 package com.rvillarroel.jobstore.rest;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.validation.constraints.Min;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.rvillarroel.jobstore.model.Job;
 import com.rvillarroel.jobstore.repository.JobRepository;
@@ -15,21 +23,37 @@ import com.rvillarroel.jobstore.repository.JobRepository;
 @Path("/jobs")
 public class JobEndPoint {
 
+	
 	@Inject 
 	private JobRepository jobRepository;
 
-	public Job getJob(Long id) {
-		return jobRepository.find(id);
-	}
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createBook(Job job, @Context UriInfo uriInfo) {
+    	job = jobRepository.create(job);
+        URI createdURI = uriInfo.getAbsolutePathBuilder().path(job.getId().toString()).build();
+        return Response.created(createdURI).build();
+    }
 
-	public Job createJob(Job job) {
-		return jobRepository.create(job);
-	}
+    @GET
+    @Path("/{id : \\d+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBook(@PathParam("id") @Min(1) Long id) {
+        Job job = jobRepository.find(id);
 
-	public void deleteJob(Long id) {
-		jobRepository.delete(id);
-	}
-	
+        if (job == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        return Response.ok(job).build();
+    }
+
+    @DELETE
+    @Path("/{id : \\d+}")
+    public Response deleteBook(@PathParam("id") @Min(1) Long id) {
+        jobRepository.delete(id);
+        return Response.noContent().build();
+    }
+    
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getJobs() {
@@ -40,9 +64,16 @@ public class JobEndPoint {
 		
 		return Response.ok(jobs).build();
 	}
-
-	public Long countJobs() {
-		return jobRepository.countAll();
+	
+	@GET
+	@Path("/count")
+	public Response countJobs() {
+				
+		Long nbOfJobs = jobRepository.countAll();
+		if (nbOfJobs == 0)
+			return Response.noContent().build();
+		
+		return Response.ok(nbOfJobs).build();
 	}
 	
 	
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO Job (id, name, description, publication_date, image_url) VALUES ( 1001, 'Electricista', 'Advanced Java EE Development for RAD Guidebook', DATE '2011-11-21', 'http://ecx.images-amazon.com/images/I/51bjnhlGbeL._SL160_.jpg')
+INSERT INTO Job (id, name, description, publication_date, image_url) VALUES ( 1002, 'Plomero', 'Advanced Java EE Development for RAD Guidebook (Nov-2011)', DATE '2011-11-21', 'http://ecx.images-amazon.com/images/I/511pqkJd3UL._SL160_.jpg')
